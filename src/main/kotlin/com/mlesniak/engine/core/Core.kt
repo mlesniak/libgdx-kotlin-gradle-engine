0c05c630d8f4139da76e5915de134202a7e7baee
Michael Lesniak <mail@mlesniak.com>
2022-10-27 12:30:01 +0200

    Refactoring

--- src/main/kotlin/com/mlesniak/engine/core/Core.kt
+++ src/main/kotlin/com/mlesniak/engine/core/Core.kt
@@ -11,19 +11,16 @@ import com.mlesniak.engine.Engine
 class Core(private val renderer: Renderer) : ApplicationAdapter() {
     private lateinit var batch: SpriteBatch
     private lateinit var pixmap: Pixmap
-    private lateinit var canvas: Canvas
-    private lateinit var texture: Texture
     private lateinit var engine: Engine
 
     override fun create() {
         batch = SpriteBatch()
-        pixmap = Pixmap(Gdx.graphics.width, Gdx.graphics.height, Pixmap.Format.RGB888)
-        canvas = Canvas(pixmap)
+        val pixmap = Pixmap(Gdx.graphics.width, Gdx.graphics.height, Pixmap.Format.RGB888)
+        val canvas = Canvas(pixmap)
         engine = Engine(canvas)
     }
 
-    override fun render() {
-        // Later on we will collect all keys and/or mouse events
+    override fun render() { // Later on we will collect all keys and/or mouse events
         // and pass them to the update function.
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             Gdx.app.exit()
@@ -32,7 +29,7 @@ class Core(private val renderer: Renderer) : ApplicationAdapter() {
         renderer.update()
         renderer.draw(engine)
 
-        texture = Texture(pixmap)
+        val texture = Texture(pixmap)
         batch.begin()
         batch.draw(texture, 0f, 0f)
         batch.end()
