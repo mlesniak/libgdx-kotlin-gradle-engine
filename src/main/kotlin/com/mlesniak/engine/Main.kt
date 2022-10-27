6b3b283b3c5976b1c743bd53c36e8c6fd9d38249
Michael Lesniak <mail@mlesniak.com>
2022-10-27 07:55:34 +0200

    Add TODOs

--- src/main/kotlin/com/mlesniak/engine/Main.kt
+++ src/main/kotlin/com/mlesniak/engine/Main.kt
@@ -1,12 +1,16 @@
 package com.mlesniak.engine
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
-
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 
+// TODO(mlesniak) Keyboard handling to close the window
+// TODO(mlesniak) Single Pixel drawing
 
-
-
+// TODO(mlesniak) For later...:
+// TODO(mlesniak) Line drawing via Bresenham
+// TODO(mlesniak) Basic 3D Vector classes
+// TODO(mlesniak) Basic Matrix classes
+// TODO(mlesniak) Simple projection and rotations
 fun main() {
     val config = Lwjgl3ApplicationConfiguration()
     config.setForegroundFPS(60)
