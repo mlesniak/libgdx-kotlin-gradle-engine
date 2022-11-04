0fd6ef8f38e6d7d7e4ab6d665f69155d702f9d1e
Michael Lesniak <mail@mlesniak.com>
2022-11-04 08:00:33 +0100

    Visualize algorithm with colors

--- src/main/kotlin/com/mlesniak/engine/EngineDemo.kt
+++ src/main/kotlin/com/mlesniak/engine/EngineDemo.kt
@@ -5,6 +5,7 @@ import com.mlesniak.engine.core.Renderer
 import com.mlesniak.engine.engine.BaseMatrix
 import com.mlesniak.engine.engine.Engine
 import com.mlesniak.engine.engine.Model
+import com.mlesniak.engine.engine.Vector
 import com.mlesniak.engine.engine.model
 import com.mlesniak.engine.engine.times
 import kotlin.math.PI
@@ -29,18 +30,24 @@ class EngineDemo : Renderer {
         val cx = engine.width() / 2
         val cy = engine.height() / 2
 
-        val scale = ((sin(angle * PI/180.0) * 200)).toFloat().absoluteValue + 100f
-        val projection =
-            BaseMatrix.translate(400f, 300f) *
-                BaseMatrix.scale(scale, scale, scale) *
-                BaseMatrix.rotateZ(angle + 180f) *
-                BaseMatrix.rotateX(angle) *
-                BaseMatrix.rotateY(angle)
-        engine.model(model, projection)
-        angle += 1f
+        // val scale = ((sin(angle * PI/180.0) * 200)).toFloat().absoluteValue + 100f
+        // val projection =
+        //     BaseMatrix.translate(400f, 300f) *
+        //         BaseMatrix.scale(scale, scale, scale) *
+        //         BaseMatrix.rotateZ(angle + 180f) *
+        //         BaseMatrix.rotateX(angle) *
+        //         BaseMatrix.rotateY(angle)
+        // engine.model(model, projection)
+        // angle += 1f
+
+        engine.triangle(
+            Vector(tick % 800, 300),
+            Vector(500, tick % 600),
+            Vector(450, (tick % 600) / 2),
+        )
 
         if (tick % 60 == 0) {
-            println(Gdx.graphics.framesPerSecond)
+            println("FPS:" + Gdx.graphics.framesPerSecond)
         }
         tick++
     }
