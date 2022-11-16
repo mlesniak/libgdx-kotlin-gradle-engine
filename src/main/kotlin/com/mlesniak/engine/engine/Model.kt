fdfc047d6a07e339896b1f94217f46c34cb2ede0
Michael Lesniak <mail@mlesniak.com>
2022-11-16 06:29:38 +0100

    Refactoring

--- src/main/kotlin/com/mlesniak/engine/engine/Model.kt
+++ src/main/kotlin/com/mlesniak/engine/engine/Model.kt
@@ -1,22 +1,23 @@
 package com.mlesniak.engine.engine
 
+import com.mlesniak.engine.engine.Model.Companion.forObject
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.regex.Pattern
-import kotlin.random.Random
 
-// This is not a good name...
 class Model(
     val vertices: List<Vector>,
     val faces: List<List<Int>>,
-    val normals: List<Vector>,
+
+    // Not sure if these are needed for our
+    // purposes anymore?
+    val vertexNormals: List<Vector>,
 ) {
     companion object {
         fun load(filename: String): Model {
             val lines = Files.readAllLines(Path.of(filename))
             val vertices = lines
-                .filter { line -> line.startsWith("v ") }
-                .map { it.split(Pattern.compile(" +")) }
+                .forObject("v")
                 .map {
                     it.subList(1, 4)
                         .map { v -> v.toFloat() }
@@ -24,8 +25,7 @@ class Model(
                 .map { plist -> Vector(plist[0], plist[1], plist[2]) }
 
             val normals = lines
-                .filter { line -> line.startsWith("vn ") }
-                .map { it.split(Pattern.compile(" +")) }
+                .forObject("vn")
                 .map {
                     it.subList(1, 4)
                         .map { v -> v.toFloat() }
@@ -33,44 +33,44 @@ class Model(
                 .map { plist -> Vector(plist[0], plist[1], plist[2]) }
 
             val faces = lines
-                .filter { line -> line.startsWith("f ") }
-                .map { it.split(Pattern.compile(" +")) }
+                .forObject("f")
                 .map {
                     it.subList(1, it.size).map { i -> i.split("/")[0].toInt() }
                 }
 
             return Model(vertices, faces, normals)
         }
+
+        private fun MutableList<String>.forObject(prefix: String) =
+            this
+                .filter { line -> line.startsWith("$prefix ") }
+                .map { it.split(Pattern.compile(" +")) }
     }
 }
 
 fun Engine.model(model: Model, projection: Matrix, wireframe: Boolean = false) {
     // Direction of the light source. In this case,
-    // going into the scene.
+    // going into the scene. Later on, this will be
+    // a property of the engine, for now this is
+    // sufficient.
     val light = Vector(0f, 0f, -1f)
-
 
     for (idx in model.faces.indices) {
         val face = model.faces[idx]
 
-        val p1 = model.vertices[face[0] - 1]
-        val p2 = model.vertices[face[1] - 1]
-        val p3 = model.vertices[face[2] - 1]
+        val p1 = projection * model.vertices[face[0] - 1]
+        val p2 = projection * model.vertices[face[1] - 1]
+        val p3 = projection * model.vertices[face[2] - 1]
 
-        val p1h = projection * p1
-        val p2h = projection * p2
-        val p3h = projection * p3
-
-
-        val normal = (p3h - p1h).cross(p2h - p1h).normalize()
+        val normal = (p3 - p1).cross(p2 - p1).normalize()
         val intensity = normal * light
         if (intensity > 0) {
             val color = (255.0 * intensity).toInt()
             val rgb = (color shl 16) or (color shl 8) or color
             if (wireframe) {
-                wireTriangle(p1h, p2h, p3h, rgb)
+                wireTriangle(p1, p2, p3, rgb)
             } else {
-                triangle(p1h, p2h, p3h, rgb)
+                triangle(p1, p2, p3, rgb)
             }
         }
     }
